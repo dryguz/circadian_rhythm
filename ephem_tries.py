733ed4e4a582a1cd08802210886d7188fd368240
dryguz <dryguz@gmail.com>
2018-08-10 21:35:33 +0200

    removed tweepy_tries.py extended ephem_tries by initial bokeh attempt

--- ephem_tries.py
+++ ephem_tries.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 # ---------------------------------------------------------------------------
 # will run loops over those values
-places = {'Paris',
+places = ['Paris',
           'Warsaw',
           'Berlin',
           'Prague',
@@ -19,7 +19,7 @@ places = {'Paris',
           'Sao Paulo',
           'Johannesburg',
           'Seoul',
-        }
+        ]
 
 dates = ['2017/01/15', '2017/02/15', '2017/03/15', '2017/04/15', '2017/05/15', 
          '2017/06/15', '2017/07/15', '2017/08/15', '2017/09/15', '2017/10/15',
@@ -52,8 +52,29 @@ for place in places:
         data.loc[date,place]=(x,y)
         
 # ---------------------------------------------------------------------------
-# Twitter part
+# Bokeh
 
-    
+from bokeh.io import output_file, show
+from bokeh.layouts import widgetbox
+from bokeh.models.widgets import Button, RadioButtonGroup, Select, Slider
+from bokeh.plotting import figure, output_file, show
 
-    
+output_file("layout_widgets.html")
+
+# create some widgets
+slider = Slider(start=1, end=12, value=1, step=.1, title="Month Slider")
+button_group = RadioButtonGroup(labels=["Rise", "Transition", "Set"], active=0)
+#select = Select(title="Option:", value="Paris", options=places)
+
+
+# put the results in a row
+show(widgetbox(slider, button_group, select, width=300))
+
+#series
+x = data.loc[:,places]
+
+#plot constructions
+output_file("overview_sun.html")
+p = figure(x_range=places)
+
+p.circle(y, places, size=9, fill_color="orange", line_color="blue", line_width=2)
